486765dce9495a3037a8c05d77af2fea4d3a952b
tofritz <tfritz@smu.edu>
2019-12-16 16:29:43 -0600

    works for single-line text replacement

--- find_and_replace/find_and_replace.py
+++ find_and_replace/find_and_replace.py
@@ -1,10 +1,11 @@
+# simple script to replace all occurences of a text string within all files of a given file extension within the folder the script it run.
+
 import fileinput
 import glob
 
-text_to_search = 'Lorem'
-replacement_text = 'Porem' 
+text_to_search = '''<script src="great success!" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"'''
+replacement_text = '!another test!'
 
 with fileinput.input(glob.glob('*.html'), inplace=True, backup='.bak') as file:
-    print('hey')
     for line in file:
         print(line.replace(text_to_search, replacement_text), end='')
