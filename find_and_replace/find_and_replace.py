75cea8adbce319d137752c219d93e8c366bbf469
tofritz <tfritz@smu.edu>
2019-12-16 18:36:25 -0600

    testing git commands

--- find_and_replace/find_and_replace.py
+++ find_and_replace/find_and_replace.py
@@ -1,11 +1,18 @@
-# simple script to replace all occurences of a text string within all files of a given file extension within the folder the script it run.
+# simple script to replace all occurences of a text string within all files of a given file extension within the folder the script is run.
+# Problems: can only edit single lines as input method iterates by line, ide indicating problems with code syntax @ line 14 but it runs.
+
+# Make sure to run using python3
 
 import fileinput
 import glob
 
-text_to_search = '''<script src="great success!" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"'''
-replacement_text = '!another test!'
+text_to_search = '''!another test!'''
+replacement_text = '!another thing to try!'
 
-with fileinput.input(glob.glob('*.html'), inplace=True, backup='.bak') as file:
-    for line in file:
-        print(line.replace(text_to_search, replacement_text), end='')
+try:
+    with fileinput.input(glob.glob('*.html'), inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace(text_to_search, replacement_text), end='')
+
+except:
+    print('ERROR')
